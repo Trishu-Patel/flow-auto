4a6fb02762b5ba3cbde96b8644926d7ea4571d83
Trishu Patel <pateltrishu28@gmail.com>
2024-12-13 23:29:00 -0500

    feat: Create board and cell class

--- main.py
+++ main.py
@@ -1,3 +1,57 @@
+class Board:
+    def __init__(self, board):
+        self.board = []
+
+        for y, row in enumerate(board):
+            board_row = []
+            for x, cell in enumerate(row):
+                board_row.append(Cell(cell, x, y))
+            self.board.append(board_row)
+
+        self.size = len(board)
+
+    def print_board(self):
+        for row in self.board:
+            for cell in row:
+                cell.print()
+            print()
+
+class Cell:
+    def __init__(self, value, x, y):
+        self.value = value
+        self.x = x
+        self.y = y
+
+    @property
+    def is_blank(self):
+        return self.value == 0
+    
+    @property
+    def color(self):
+        colors = [
+            (0, 0, 0),      # blank
+            (255, 0, 0),    # red
+            (0, 255, 0),    # green
+            (0, 0, 255),    # blue
+            (255, 255, 0),  # yellow
+            (255, 165, 0),  # orange
+        ]
+
+        return colors[self.value]
+    
+    def print(self):
+        if self.value == 0:
+            print("  ", end="")
+            return
+        
+        red = self.color[0]
+        green = self.color[1]
+        blue = self.color[2]
+
+        colored_squared = f"\033[48;2;{red};{green};{blue}m  \033[0m"
+        print(colored_squared, end="")
+
+    
 board = [
     [1, 0, 2, 0, 4],
     [0, 0, 3, 0, 5],
@@ -6,34 +60,7 @@ board = [
     [0, 1, 3, 5, 0],
 ]
 
-def print_cell(cell):
-    colors = [
-        (0, 0, 0),      # blank
-        (255, 0, 0),    # red
-        (0, 255, 0),    # green
-        (0, 0, 255),    # blue
-        (255, 255, 0),  # yellow
-        (255, 165, 0),  # orange
-    ]
-
-    if cell == 0:
-        print("  ", end="")
-        return
-
-    cell_color = colors[cell] 
-
-    red = cell_color[0]
-    green = cell_color[1]
-    blue = cell_color[2]
-
-    colored_squared = f"\033[48;2;{red};{green};{blue}m  \033[0m"
-    print(colored_squared, end="")
-
-def print_board(board):
-    for row in board:
-        for cell in row:
-            print_cell(cell)
-        print()
-
 if __name__ == "__main__":
-    print_board(board)
+    board = Board(board)
+
+    board.print_board()
