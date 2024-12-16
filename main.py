bf3c40f23a64dc9ff35e89839a4534a5f671cb7b
Trishu Patel <pateltrishu28@gmail.com>
2024-12-15 20:34:05 -0500

    refactor: Simplify next cell logic

--- main.py
+++ main.py
@@ -1,66 +1,117 @@
-class Board:
+from PIL import Image, ImageDraw
+
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+
+
+class Cell:
+    def __init__(self, color, x, y, board):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.board = board
+
+    @property
+    def top_cell(self):
+        return self.board.get_cell(self.x, self.y - 1)
+
+    @property
+    def left_cell(self):
+        return self.board.get_cell(self.x - 1, self.y)
+
+    @property
+    def bottom_cell(self):
+        return self.board.get_cell(self.x, self.y + 1)
+
+    @property
+    def right_cell(self):
+        return self.board.get_cell(self.x + 1, self.y)
+
+    def print(self):
+        r = self.color[0]
+        g = self.color[1]
+        b = self.color[2]
+
+        print(f"\033[48;2;{r};{g};{b}m  \033[0m", end="")
+
+        print(f" ({self.x}, {self.y})")
+
+    def deep_print(self):
+        r = self.color[0]
+        g = self.color[1]
+        b = self.color[2]
+
+        print(f"\033[48;2;{r};{g};{b}m  \033[0m", end="")
+
+        print(f" ({self.x}, {self.y})")
+
+        print("top: ", end="")
+        if self.top_cell != None:
+            self.top_cell.print()
+        else:
+            print("None")
+
+        print("left: ", end="")
+        if self.left_cell != None:
+            self.left_cell.print()
+        else:
+            print("None")
+
+        print("bottom: ", end="")
+        if self.bottom_cell != None:
+            self.bottom_cell.print()
+        else:
+            print("None")
+
+        print("right: ", end="")
+        if self.right_cell != None:
+            self.right_cell.print()
+        else:
+            print("None")
+
+
+class Free_Flow:
     def __init__(self, board):
         self.board = []
+        self.size = len(board)
 
         for y, row in enumerate(board):
             board_row = []
-            for x, cell in enumerate(row):
-                board_row.append(Cell(cell, x, y))
+            for x, color in enumerate(row):
+                cell = Cell(color, x, y, self)
+                board_row.append(cell)
             self.board.append(board_row)
 
-        self.size = len(board)
+    def get_cell(self, x: int, y: int) -> Cell | None:
+        if x < 0 or x >= self.size:
+            return None
 
-    def print_board(self):
+        if y < 0 or y >= self.size:
+            return None
+
+        return self.board[y][x]
+
+    def print(self):
         for row in self.board:
             for cell in row:
                 cell.print()
             print()
 
-class Cell:
-    def __init__(self, value, x, y):
-        self.value = value
-        self.x = x
-        self.y = y
-
-    @property
-    def is_blank(self):
-        return self.value == 0
-    
-    @property
-    def color(self):
-        colors = [
-            (0, 0, 0),      # blank
-            (255, 0, 0),    # red
-            (0, 255, 0),    # green
-            (0, 0, 255),    # blue
-            (255, 255, 0),  # yellow
-            (255, 165, 0),  # orange
-        ]
-
-        return colors[self.value]
-    
-    def print(self):
-        if self.value == 0:
-            print("  ", end="")
-            return
-        
-        red = self.color[0]
-        green = self.color[1]
-        blue = self.color[2]
-
-        colored_squared = f"\033[48;2;{red};{green};{blue}m  \033[0m"
-        print(colored_squared, end="")
-
-    
-board = [
-    [1, 0, 2, 0, 4],
-    [0, 0, 3, 0, 5],
-    [0, 0, 0, 0, 0],
-    [0, 2, 0, 4, 0],
-    [0, 1, 3, 5, 0],
-]
 
 if __name__ == "__main__":
-    board = Board(board)
 
-    board.print_board()
+    board = [
+        [BLUE, RED, RED, RED, ORANGE],
+        [BLUE, RED, YELLOW, YELLOW, ORANGE],
+        [BLUE, RED, YELLOW, ORANGE, ORANGE],
+        [BLUE, RED, ORANGE, ORANGE, GREEN],
+        [BLUE, BLUE, GREEN, GREEN, GREEN],
+    ]
+
+    game = Free_Flow(board)
+
+    game.get_cell(0, 0).deep_print()
